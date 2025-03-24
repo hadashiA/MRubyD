d6623212f1d104ebf0de8d446537d55c4899366d
hadashiA <dev@hadashikick.jp>
2025-03-24 11:36:26 +0900

    Remove debug

--- sandbox/MRubyD.Benchmark/Program.cs
+++ sandbox/MRubyD.Benchmark/Program.cs
@@ -1,25 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using System.Reflection;
+﻿using System.Reflection;
 using BenchmarkDotNet.Running;
-using MRubyD.Benchmark;
-
 
 BenchmarkSwitcher.FromAssembly(Assembly.GetEntryAssembly()!).Run(args);
-
-unsafe
-{
-    var mrb = NativeMethods.MrbOpen();
-    var source = """
-                 def fib n
-                   return n if n < 2
-                   fib(n-2) + fib(n-1)
-                 end
-                 fib(37)
-                 """u8;
-    nint sourceLength = source.Length;
-    fixed (byte* sourcePtr = source)
-    {
-        var result = NativeMethods.MrbLoadNString(mrb, sourcePtr, sourceLength);
-    }
-}
